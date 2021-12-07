2d62bf5d64c3ec6822608993838821c553aac637
athiraPrajod <athiraprajod1@gmail.com>
2021-12-07 17:58:37 +0530

    mern-1st

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import MyHome from "./components/login/MyHome";
+
+function App() {
+  return (
+    <div className="App">
+      <MyHome />
+
+      <div className="container text-center">
+        <br></br>
+        <br></br>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/HomePage/HomePage.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import "./HomePageStyles.css";
+//import airport from "./airport.png";
+import image1 from "./image1.jpg";
+import image2 from "./image2.jpg";
+import { Link } from "react-router-dom";
+
+export class HomePage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [],
+      tours: []
+    };
+  }
+
+  async componentDidMount() {
+    return fetch("http://localhost:5000/view")
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson);
+        this.setState({
+          tours: responseJson,
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+
+  render() {
+    return (
+    
+      <div className="container">
+        <br></br>
+        <div className={"justify-content-center mt-5 mb-5"}>
+          <h1 className="rr">
+            {/* <img src={airport}></img> */}
+            &nbsp;WELCOME!
+          </h1>
+        </div>
+        <br></br>
+        <div className="container-text-left">
+          <h2>Amazing adventures await
+            <br></br>
+            Choose a country to get started :)
+          </h2>
+        </div>
+
+        <br></br>
+
+        <div className="container text-center">
+          <img src={image1}></img>
+        </div>
+        <br></br>
+        <div className="container-text-left">
+          <h2>
+            ---Cards here---
+          </h2>
+          <div class="row">
+          {this.state.tours.map((value, key) => (
+            <div className="card" key={key}>
+              <img
+                src="https://i.pinimg.com/originals/54/2c/7e/542c7e0c990d70f3e732b8a9cf64815f.png"
+                className="card-img-top"
+              />
+              <div className="card-body">
+                <h5 className="card-title">{value.tname}</h5>
+                <p className="card-text justufy">{value.tdescription}</p>
+                <br />
+                <p className="card-text mb-0 p-8">
+                  <b>Price: </b>
+                  {value.price}
+                </p>
+                <p className="card-text">
+                  <b>Package No: </b>
+                  {value.tnumber}
+                </p>
+                <Link to="/add">
+                <div class="text-center">
+                  <a href="#" class="btn btn-primary">
+                    Book Now
+                  </a>
+                </div>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+          <br></br>
+          <p>
+            End of cards
+          </p>
+        </div>
+
+        <div className="container-text-left">
+          <h2></h2>
+          <br></br>
+          <p>
+            
+          </p>
+        </div>
+
+        <br></br>
+        <div className="container text-center">
+          <img src={image2}></img>
+        </div>
+
+        <br></br>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
